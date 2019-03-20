62fa38b9c578c91316f11c41f182150c214377d5
Cathy Wong <wong.catherine.mail@gmail.com>
2019-03-20 14:04:58 -0400

    Factor out puddleworld ontology

---
+++ utils.py
@@ -0,0 +1,22 @@
+"""
+utils.py
+"""
+#### If need to add adjacent libraries.
+import sys
+sys.path.insert(0, "../")
+sys.path.insert(0, "../pyccg")
+sys.path.insert(0, "../pyccg/nltk")
+####
+
+import ec.type as ec_type
+from pyccg.logic import TypeSystem, Ontology, Expression
+
+
+### Utilities to convert PyCCG ontologies into Dreamcoder primitives.
+
+
+if __name__ == "__main__":
+	print("Demo: puddleworld ontology conversion.")
+
+	from puddleworldOntology import ontology
+
