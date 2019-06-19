79074e830e5d18d6eba8596116d57e5594dc0e9f
Cathy Wong <wong.catherine.mail@gmail.com>
2019-06-19 18:26:34 -0400

    Skeleton for wake_generative_with_pyccg in an EC language learner

--- ecLearner.py
+++ ecLearner.py
@@ -17,6 +17,7 @@ import random
 import string
 
 from ec import explorationCompression, commandlineArguments, Task, ecIterator
+from enumeration import * # EC enumeration.
 from taskRankGraphs import plotEmbeddingWithLabels
 from grammar import Grammar
 from utilities import eprint, numberOfCPUs
@@ -26,7 +27,7 @@ from task import *
 from puddleworldOntology import ec_ontology, process_scene
 from utils import convertOntology
 
-#### Load and prepare dataset for EC.
+#### Utility functions to load and prepare dataset for EC.
 def loadPuddleWorldTasks(datafile='data/puddleworld.json'):
     """
     Loads a pre-processed version of the Puddleworld tasks.
@@ -86,10 +87,11 @@ def makeTinyTasks(input_type, output_type, num_tiny=1, tiny_scene_size=2):
     train, test = [makeTinyTask(tiny_scene_size) for _ in range(num_tiny)], [makeTinyTask(tiny_scene_size) for _ in range(num_tiny)]
     return train, []
 
-
-
-### Basic recurrent feature extractor for the instruction strings.
 class InstructionsFeatureExtractor(RecurrentFeatureExtractor):
+    """
+    InstructionsFeatureExtractor: minimal EC-recogntition-model feature extractor for the instruction strings.
+    """
+
     def _tokenize_string(self, features):
         """Ultra simple tokenizer. Removes punctuation, then splits on spaces."""
         remove_punctuation = str.maketrans('', '', string.punctuation)
@@ -124,8 +126,68 @@ class InstructionsFeatureExtractor(RecurrentFeatureExtractor):
                                                             bidirectional=True,
                                                             cuda=cuda)
 
+class ECLanguageLearner:
+    """
+    ECLanguageLearner: driver class that manages learning between PyCCG and EC.
+    """
+    def __init__(self,
+                pyccg_learner):
+                self.pyccg_learner = pyccg_learner
 
-### Run the learner.
+    
+    def _update_pyccg_with_distant_batch(self, tasks, timeout):
+        """
+        Ret:
+            pyccg_meanings: dict from task -> PyCCG meanings for the sentence, 
+                            or None if no expression was found.
+        """
+        return None
+
+    def _pyccg_meanings_to_ec_frontiers(self, pyccg_meanings):
+        """
+        Ret:
+            pyccg_frontiers: dict from task -> Dreamcoder frontiers.
+        """
+        return None
+
+    def wake_generative_with_pyccg(self,
+                    grammar, tasks, 
+                    maximumFrontier=None,
+                    enumerationTimeout=None,
+                    CPUs=None,
+                    solver=None,
+                    evaluationTimeout=None):
+        """
+        Dreamcoder wake_generative using PYCCG enumeration to guide exploration.
+
+        Enumerates from PyCCG with a timeout and blindly from the EC grammar.
+        Updates PyCCG using both sets of discovered meanings.
+        Converts the meanings into EC-style frontiers to be handed off to EC.
+        """
+        # Enumerate PyCCG meanings and update the word learner.
+        pyccg_meanings = self._update_pyccg_with_distant_batch(tasks, enumerationTimeout)
+
+        # Enumerate the remaining tasks using EC-style blind enumeration.
+        unsolved_tasks = [task for task in tasks if pyccg_meanings[task] is None]
+        fallback_frontiers, fallback_times = multicoreEnumeration(grammar, tasks, 
+                                                   maximumFrontier=maximumFrontier,
+                                                   enumerationTimeout=enumerationTimeout,
+                                                   CPUs=CPUs,
+                                                   solver=solver,
+                                                   evaluationTimeout=evaluationTimeout)
+        # Update PyCCG model with fallback discovered frontiers.
+                # TODO(catwong): Just create the SExpressions here since they aren't 'meanings'.
+                # TODO: just update_with_supervised one at a time in a loop.
+
+        # Convert and consolidate PyCCG meanings and fallback frontiers for handoff to EC.
+        pyccg_frontiers = self._pyccg_meanings_to_ec_frontiers(pyccg_meanings)
+        all_frontiers = {t : pyccg_frontiers[t] if t in pyccg_frontiers else fallback_frontiers[t]}
+        all_times = {t : enumerationTimeout if t in pyccg_frontiers else fallback_times[t]}
+
+        return all_frontiers, all_times
+            
+
+### Additional command line arguments for Puddleworld.
 def puddleworld_options(parser):
     parser.add_argument(
         "--local",
@@ -178,21 +240,21 @@ if __name__ == "__main__":
         featureExtractor=InstructionsFeatureExtractor,
         extras=puddleworld_options)
 
-    checkpoint_analysis = args.pop("checkpoint_analysis")
+    checkpoint_analysis = args.pop("checkpoint_analysis") # EC checkpoints need to be run out of their calling files, so this is here.
 
     """Run the EC learner."""
     if checkpoint_analysis is None:
-        # Set up.
+        # Set up output directories.
         random.seed(args.pop("random_seed"))
         timestamp = datetime.datetime.now().isoformat()
         outputDirectory = "experimentOutputs/puddleworld/%s"%timestamp
         os.system("mkdir -p %s"%outputDirectory)
 
-        # Convert ontology.
+        # Convert pyccg ontology -> Dreamcoder.
         puddleworldTypes, puddleworldPrimitives = convertOntology(ec_ontology)
         input_type, output_type = puddleworldTypes['model'], puddleworldTypes['action']
 
-        # Make tasks.
+        # Convert sentences-scenes -> Dreamcoder style tasks.
         doLocal, doGlobal, doTiny= args.pop('local'), args.pop('global'), args.pop('tiny')
         num_tiny, tiny_size = args.pop('num_tiny'), args.pop('tiny_scene_size')
 
@@ -205,11 +267,15 @@ if __name__ == "__main__":
         eprint("Using tiny tasks of size %d: %d train, %d test" % (tiny_size, len(tinyTrain), len(tinyTest)))
         eprint("Using total tasks: %d train, %d test" % (len(allTrain), len(allTest)))
 
-        # Make grammar.
+        # Make Dreamcoder grammar.
         baseGrammar = Grammar.uniform(puddleworldPrimitives)
         print(baseGrammar.json())
-        
-        # Run EC.
+
+        # Initialize the language learner driver.
+        # (note: cathy - to debug, jankily run with made up arguments, but we don't actually wanna do that,)
+
+
+        # Run Dreamcoder exploration/compression.
         explorationCompression(baseGrammar, allTrain, 
                                 testingTasks=allTest, 
                                 outputPrefix=outputDirectory, **args)
